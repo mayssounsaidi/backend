9d71e0c2a14dea88459b6977278f4c197f26fd2c
mayssounsaidi <mayssounsaidi1@gmail.com>
2025-12-05 18:24:31 +0100

    commit 6

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.listen(PORT, () => {
 });  
 
 require('dotenv').config();
-const mongoose = require('mongoose');
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connecté '))
